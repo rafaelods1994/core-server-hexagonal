70fa4b1c85c5cc43b8896776dcbcbe8e7ca80fdb
unknown <rafaelods1994@gmail.com>
2025-03-20 10:53:50 +1300

    Initial commit with base project structure

---
+++ src/main/java/com/rafael/hexagonal/CoreServeApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreServeApplication.class, args);
+	}
+
+}
